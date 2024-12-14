3fa76eebd590df4bd0c3958bc5007d6a93313495
Marc <mmarc471@gmail.com>
2024-12-14 11:10:50 +0100

    Fixed overlayfs command when mods have spaces in the name

--- subprojects/lib_mod_manager/src/overlayfs.c
+++ subprojects/lib_mod_manager/src/overlayfs.c
@@ -7,24 +7,18 @@
 #include <stdio.h>
 
 overlay_errors_t overlay_mount(char ** sources, const char * dest, const char * upperdir, const char * workdir) {
-	g_autofree char * lower_dir = g_strjoinv(":", sources);
-	g_autofree char * mount_data = g_strjoin("", "lowerdir=", lower_dir, ",workdir=", workdir, ",upperdir=", upperdir, NULL);
-
-	printf("\n\n\n\n%s -- %s\n\n\n\n", lower_dir, mount_data);
+	g_autofree char * lower_dir = g_strjoinv("\":\"", sources);
+	g_autofree char * mount_data = g_strjoin("", "lowerdir=\"", lower_dir, "\",workdir=\"", workdir, "\",upperdir=\"", upperdir, "\"", NULL);
+	g_autofree char * full_command = g_strjoin(" ", "fuse-overlayfs", "-o", mount_data, dest, NULL);
 
 	overlay_errors_t result = SUCCESS;
 
+	printf("%s\n", dest);
+
 	if(access("/usr/bin/fuse-overlayfs", F_OK) == 0) {
-		int pid = fork();
-		if(pid == 0) {
-			//exit is used to get the return value when using waitpid
-			exit(execl("/usr/bin/fuse-overlayfs", "fuse-overlayfs", "-o", mount_data, dest, NULL));
-		} else {
-			int return_value = 0;
-			waitpid(pid, &return_value, 0);
-			if(return_value != 0) {
-				result = FAILURE;
-			}
+		int return_value = system(full_command);
+		if(return_value != 0) {
+			result = FAILURE;
 		}
 	} else {
 		result = NOT_INSTALLED;
