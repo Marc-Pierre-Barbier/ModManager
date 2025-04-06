e0e9a8087fa0706cbc6f847ee9e74883b4389084
Marc <mmarc471@gmail.com>
2025-04-06 23:14:24 +0200

    refactor steam_hook

--- subprojects/steam_hook/src/main.rs
+++ subprojects/steam_hook/src/main.rs
@@ -15,22 +15,40 @@ fn replace_path_at_common(path: std::path::PathBuf, new_path: std::path::PathBuf
 
 
 fn main() {
-	let steam_appid_string = env::args().rfind(|f| f.contains("AppId=")).expect("[MOD_MANAGER] This is not steam, We are expecting to see AppId=VALUE in the args");
-	let appid_string = steam_appid_string.split("AppId=").nth(1).expect("[MOD_MANAGER] Invalid appid format");
-	let appid: i32 = appid_string.parse().expect("[MOD_MANAGER] Invalid appid format");
-	let is_deployed = binding::bind_is_deployed(appid).expect("[MOD_MANAGER] Failed to fetch the status");
+	let steam_appid_string = env::args()
+		.rfind(|f| f.contains("AppId="))
+		.expect("[MOD_MANAGER] This is not steam, We are expecting to see AppId=VALUE in the args");
 
-	let game_path_index = env::args().position(|f| f == "waitforexitandrun").expect("[MOD_MANAGER] Failed to find GamePath index") + 1;
+	let appid_string = steam_appid_string.split("AppId=")
+		.nth(1)
+		.expect("[MOD_MANAGER] Invalid appid format");
+
+	let appid: i32 = appid_string.parse()
+		.expect("[MOD_MANAGER] Invalid appid format");
+
+	let is_deployed = binding::bind_is_deployed(appid)
+		.expect("[MOD_MANAGER] Failed to fetch the status");
+
+	let game_path_index = env::args()
+		.position(|f| f == "waitforexitandrun")
+		.expect("[MOD_MANAGER] Failed to find GamePath index") + 1;
+
 	//The path where the game expect to start
-	let steam_game_path = env::args().nth(game_path_index).expect("[MOD_MANAGER] Could not extract game path");
+	let steam_game_path = env::args()
+		.nth(game_path_index)
+		.expect("[MOD_MANAGER] Could not extract game path");
+
 	//The path where we deployed
 	let game_path = binding::bind_get_deploy_target(appid);
-
-	let new_path = replace_path_at_common(std::path::PathBuf::from(steam_game_path), game_path);
+	let new_path = replace_path_at_common(std::path::PathBuf::from(steam_game_path), game_path.clone());
 
 	//replace the arg where the path is stored by our altered path
-	let mut child_args: Vec<_> = env::args().skip(1).collect(); //skip ourselves
-	child_args[game_path_index - 1] = new_path.to_str().expect("[MOD_MANAGER] Failed to process game args").to_string();
+	let mut child_args: Vec<_> = env::args()
+		.skip(1)
+		.collect(); //skip ourselves
+
+	child_args[game_path_index - 1] = new_path.to_str()
+		.expect("[MOD_MANAGER] Failed to process game args").to_string();
 
 	println!("[MOD_MANAGER] Edited game args: {}", child_args.join(" "));
 	println!("[MOD_MANAGER] Current deployment status: {}", is_deployed);
@@ -57,6 +75,7 @@ fn main() {
 	let steam_launcher = child_args.remove(0);
 	let exit_code = Command::new(steam_launcher)
 		.args(child_args)
+		.current_dir(game_path)
 		.spawn()
 		.expect("[MOD_MANAGER] Failed to run the child")
 		.wait()
